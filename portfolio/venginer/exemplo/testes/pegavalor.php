73d2f1f9bacbcac1e621eaba3eaafb61f833a3d0
paulosergioduff <paulosergioduff@gmail.com>
2018-06-28 21:42:37 -0300

    Adição de venginer no portfolio

--- portfolio/venginer/exemplo/testes/pegavalor.php
+++
@@ -1,7 +0,0 @@
-<?php
-$obj = simplexml_load_file("core.xml");
-$name = $obj->sys->name;
-$autor = $obj->sys->author;
-echo $name . '<br>';
-echo $autor;
-?>
--- portfolio/venginer/exemplo/testes/provedor.php
+++
@@ -1,5 +0,0 @@
-<?php
-$ip = getenv("REMOTE_ADDR");
-$provedor = gethostbyaddr("$ip");
-echo "O seu IP é $ip e o seu provedor é $provedor";
-?>
