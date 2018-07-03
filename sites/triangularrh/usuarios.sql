073477b80e4f24dd270a8b99eab6a86826bcd15d
paulosergioduff <paulosergioduff@gmail.com>
2018-07-03 01:38:13 -0300

    Relocação de pastas

---
+++ sites/triangularrh/usuarios.sql
@@ -0,0 +1,15 @@
+Create table usuarios (
+ID Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+login Varchar(30),
+senha Varchar(40),
+empresa Varchar(40),
+cnpj Varchar(40),
+ramo Varchar(40),
+solicitante Varchar(40),
+telefone Varchar(40),
+email Varchar(40),
+endereco Varchar(40),
+bairro Varchar(40),
+cidade Varchar(40),
+ref Varchar(40),
+Primary Key (ID)) ENGINE = MyISAM;                                                                                                                                                                                                
