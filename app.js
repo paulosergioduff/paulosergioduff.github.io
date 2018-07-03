83210af168856584376fa8b827edcdfa96714367
paulosergioduff <paulosergioduff@gmail.com>
2018-07-03 11:24:46 -0300

    Lançamento do site

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
 
       function verProjeto($pagina){
       	$( "#projeto" ).load( "includes/"+$pagina+".html" );
+      	document.getElementById('quero').innerHTML = '<center><a class="btn btn-large btn-primary" href="orcamento/index.html" style="margin: 20px;">Quero algo assim</a><a class="btn btn-large btn-warning" href="orcamento/index.html">Quero algo diferente disso</a></center>';
       	irParaTopo();
       }
 
